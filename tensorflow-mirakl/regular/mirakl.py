fc4f1f4a35882905888f9ab195084cf24942ce9c
ablanchard <ablanchard@excilys.com>
2017-06-21 00:32:37 +0200

    FFix stuff

--- tensorflow-mirakl/regular/mirakl.py
+++ tensorflow-mirakl/regular/mirakl.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""DNNRegressor with custom input_fn for Housing dataset."""
+"""DNNRegressor with custom input_fn for Mirakl dataset."""
 
 from __future__ import absolute_import
 from __future__ import division
